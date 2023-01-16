cd63c72651e5207c448f9ae52817e84770407e9b
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2023-01-16 18:25:16 +0200

    add css to navLink

--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
-// import { useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { format } from 'date-fns';
 import {
   CardItem,
   CardText,
@@ -8,21 +7,13 @@ import {
   CardImg,
   CardBox,
   CardTime,
+  ReadMoreLink,
 } from './CardItemStyled';
-// import { getIsLoading } from 'redux/selectors';
-// import Notiflix from 'notiflix';
+
 export const Card = ({
   card: { imageURL, title, content, url, description, publishedAt },
 }) => {
-  //   const [loadingId, setLoadingId] = useState(null);
-  //   const dispatch = useDispatch();
-  //   const isLoading = useSelector(getIsLoading);
-
-  const localDate = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: '2-digit',
-  }).format(new Date(publishedAt));
+  const localDate = format(new Date(publishedAt), 'MMMM do, yyyy');
 
   return (
     <CardItem>
@@ -32,9 +23,10 @@ export const Card = ({
         <CardImg src={'https://via.placeholder.com/400x220'} />
       )}
       <CardBox>
-        <CardTime>{localDate}</CardTime>
+        <CardTime>{localDate.toString()}</CardTime>
         <CardTitle dangerouslySetInnerHTML={{ __html: title }} />
         <CardText dangerouslySetInnerHTML={{ __html: description }} />
+        <ReadMoreLink to="/article">Read more</ReadMoreLink>
       </CardBox>
     </CardItem>
   );
--- src/components/CardItem/CardItemStyled.js
+++ src/components/CardItem/CardItemStyled.js
@@ -1,14 +1,19 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 export const CardItem = styled.li`
   width: 400px;
   height: 530px;
-  margin-bottom: 45px;
   background: #ffffff;
   border: 1px solid #eaeaea;
   box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.05);
   border-radius: 5px;
+  margin-bottom: 45px;
 `;
 export const CardBox = styled.div`
+  height: calc(100% - 265px);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   padding: 25px;
 `;
 export const CardText = styled.p`
@@ -16,8 +21,8 @@ export const CardText = styled.p`
   font-weight: 400;
   font-size: 16px;
   line-height: 1.5;
-
   color: #363636;
+  margin: 0;
 `;
 export const CardTitle = styled.p`
   font-family: 'Montserrat';
@@ -25,9 +30,28 @@ export const CardTitle = styled.p`
   font-size: 24px;
   line-height: 1.21;
   color: #363636;
+  margin: 0;
 `;
 export const CardImg = styled.img`
   width: 400px;
   height: 220px;
 `;
-export const CardTime = styled.p``;
+export const CardTime = styled.p`
+  font-family: 'Montserrat';
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.5;
+  color: #363636;
+  opacity: 0.6;
+  margin: 0;
+`;
+
+export const ReadMoreLink = styled(NavLink)`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 1.5;
+  color: #363636;
+  text-decoration: none;
+`;
